640a0ea22a9140cb1ae7254257e5c92403ea3725
hayato_52b <a99246780@icloud.com>
2025-05-20 18:33:24 +0900

    ADD:Front 簡単なディレクトリを作成

---
+++ front/src/features/timer/IndexTimer.tsx
@@ -0,0 +1,11 @@
+import Timer from "./components/Timer";
+import Todos from "./components/Todos";
+
+export default function IndexTimer() {
+  return(
+    <>
+      <Timer />
+      <Todos />
+    </>
+  )
+}
---
+++ front/src/features/timer/components/Timer.tsx
@@ -0,0 +1,12 @@
+export default function Timer() {
+  return(
+    <>
+      {/* Todoの名前 */}
+      {/* タイマー */}
+      {/* start/stop ボタン */}
+      {/* 保存ボタン */}
+
+      {/* Todo一覧 */}
+    </>
+  )
+}
