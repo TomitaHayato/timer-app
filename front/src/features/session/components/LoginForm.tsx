e0aad6541f5e4abec40e3269182baf7e1c37b971
hayato_52b <a99246780@icloud.com>
2025-05-24 04:53:25 +0900

    AdD:Front フォームにバリデーションを追加

--- front/src/features/session/components/LoginForm.tsx
+++ front/src/features/session/components/LoginForm.tsx
@@ -1,16 +1,40 @@
+import { useForm } from "react-hook-form"
+import FormErrorText from "./FormErrorText"
+
+type formParams = {
+  name: string,
+  email: string,
+  password: string,
+  password_confirmation: string,
+}
+
 export function LoginForm() {
+  const { register, handleSubmit, formState: { errors }, } = useForm()
+  
+  const onSubmit = (data: formParams) => {
+    console.log(data)
+  }
+
   return(
     <>
       <fieldset className="fieldset rounded-box w-full p-4">
         <legend className="fieldset-legend text-2xl">Login</legend>
 
-        <label className="label">Email</label>
-        <input type="email" className="input w-full" placeholder="Email" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="label">Email</label>
+          <FormErrorText errorText={errors.email?.message}/>
+          <input type="email" className="input w-full" placeholder="Email"
+            {...register('email', {required: '！メールアドレスを入力してください'})}
+            />
 
-        <label className="label">Password</label>
-        <input type="password" className="input w-full" placeholder="Password" />
+          <label className="label">Password</label>
+          <FormErrorText errorText={errors.password?.message}/>
+          <input type="password" className="input w-full" placeholder="Password"
+            {...register('password', {required: '！パスワードを入力してください'})}
+            />
 
-        <button className="btn btn-success mt-4">Login</button>
+          <input type="submit" className="btn btn-success btn-block mt-4" />
+        </form>
       </fieldset>
     </>
   )
