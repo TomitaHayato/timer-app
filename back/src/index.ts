218f1f14e980b03778d56db18c5241723807923f
hayato_52b <a99246780@icloud.com>
2025-05-25 22:39:47 +0900

    ADD*Back typescriptを使用できるように修正

---
+++ back/src/index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response, NextFunction } from 'express'
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const port = process.env.PORT;
+const app = express();
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send('Hello express');
+})
+
+app.listen(port, () => {
+  console.log(`サーバ起動 port:${port}`)
+})
