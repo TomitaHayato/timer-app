f311a8e474813df82456e95aacf1923f975bcbc2
hayato_52b <a99246780@icloud.com>
2025-06-01 18:23:10 +0900

    ADD:Back ユーザー新規登録の入力値に対するバリデーションを定義

---
+++ back/src/middlewares/validators/hundleValidationResult.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+
+export const hundleValidationResult = (req: Request, res: Response, next: NextFunction) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    res.status(422).json({ errors: errors.array() });
+    return;
+  }
+
+  next();
+}
--- back/src/routes/auth/authRouter.ts
+++ back/src/routes/auth/authRouter.ts
@@ -1,9 +1,14 @@
-import { NextFunction, Response, Router } from "express";
+import { Router } from "express";
 import { authCheck } from "../../middlewares/auth/auth";
 import { getDefault } from "../../controllers/users/users";
+import { signUp } from "../../controllers/auth/auth";
+import { userPostValidator } from "../../middlewares/validators/users";
+import { hundleValidationResult } from "../../middlewares/validators/hundleValidationResult";
 
 const router = Router()
 
 router.get('/', authCheck, getDefault);
+
+router.post('/signup', userPostValidator, hundleValidationResult, signUp);
 
 export default router
