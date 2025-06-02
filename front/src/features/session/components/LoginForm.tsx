2216b6e2d3ceaceacff4c7af1a9a20c014bcba11
hayato_52b <a99246780@icloud.com>
2025-06-02 22:08:18 +0900

    ADD:Front ログイン・新規登録リクエストを送信する処理を実装

--- front/src/features/session/components/LoginForm.tsx
+++ front/src/features/session/components/LoginForm.tsx
@@ -1,18 +1,28 @@
 import { useForm } from "react-hook-form"
 import FormErrorText from "./FormErrorText"
+import { clientCredentials } from "../../../utils/axios"
+import { useState } from "react";
 
-type formParams = {
-  name: string,
+type FormParams = {
   email: string,
   password: string,
-  password_confirmation: string,
 }
 
 export function LoginForm() {
-  const { register, handleSubmit, formState: { errors }, } = useForm()
+  const { register, handleSubmit, formState: { errors }, } = useForm<FormParams>();
+  // ログインに成功したかどうか
+  const [ isFailed, setIsFailed ] = useState<boolean>(false);
   
-  const onSubmit = (data: formParams) => {
-    console.log(data)
+  const onSubmit = async (data: FormParams) => {
+    console.log('送信データ：', data);
+    try {
+      const res = await clientCredentials.post('/auth/signin', data);
+      console.log('レスポンス：', res.data);
+      setIsFailed(false);
+    } catch {
+      console.error('ログインに失敗しました');
+      setIsFailed(true);
+    }
   }
 
   return(
@@ -21,8 +31,10 @@ export function LoginForm() {
         <legend className="fieldset-legend text-2xl">Login</legend>
 
         <form onSubmit={handleSubmit(onSubmit)}>
+          { isFailed && <p className="text-error text-center text-lg font-semibold">ログインに失敗しました</p> }
+
           <label className="label">Email</label>
-          <FormErrorText errorText={errors.email?.message}/>
+          <FormErrorText errorText={ errors.email?.message }/>
           <input type="email" className="input w-full" placeholder="Email"
             {...register('email', {required: '！メールアドレスを入力してください'})}
             />
@@ -33,7 +45,7 @@ export function LoginForm() {
             {...register('password', {required: '！パスワードを入力してください'})}
             />
 
-          <input type="submit" className="btn btn-success btn-block mt-4" />
+          <input type="submit" className="btn btn-success btn-block mt-4" value={"ログイン"} />
         </form>
       </fieldset>
     </>
--- front/src/utils/axios.ts
+++ front/src/utils/axios.ts
@@ -4,3 +4,9 @@ export const client = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 1000,
 })
+
+export const clientCredentials = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 1000,
+  withCredentials: true,
+})
