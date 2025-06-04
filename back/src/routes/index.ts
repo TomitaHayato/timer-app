078bd2f2a542cd5238afef16c12c3ce863334df7
hayato_52b <a99246780@icloud.com>
2025-06-05 03:41:52 +0900

    ADD;Back 　設定値の作成・更新処理を行うRouteを定義

--- back/src/routes/index.ts
+++ back/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import authRouter from "./auth/authRouter"
 import todosRouter from './todos/todosRouter'
+import settingsRouter from './settings/settingsRouter'
 
 const routers = Router();
 
 routers.use('/auth', authRouter);
 routers.use('/todos', todosRouter);
+routers.use('/settings', settingsRouter);
 
 export default routers
