0414009c9679632fc8667a0464721ad2cb822bd3
hayato_52b <a99246780@icloud.com>
2025-06-05 18:57:21 +0900

    Update:Back 不要なコントローラを削除

--- back/src/controllers/users/users.ts
+++ back/src/controllers/users/users.ts
@@ -1,26 +1,8 @@
 import { NextFunction, Request, Response } from "express";
-import { defaultAuth } from "../../config/defaultVals/defaultUser"
-import { defaultSetting } from "../../config/defaultVals/defaultSetting";
-import { defaultTodos } from "../../config/defaultVals/defaultTodos";
-import { defaultRecords } from "../../config/defaultVals/defaultRecords";
 import { getUserIdFromRequest } from "../utils/getUserId";
 import { deleteUserById } from "../../models/users/users";
 import { dbQueryHandler } from "../../models/utils/errorHandler";
 import { clearJwtCookie } from "../../utils/jwt";
-
-// 未ログイン時にデフォルトのユーザー情報を返す。
-export const getDefault = (req: Request, res: Response) => {
-  if(req.isAuthenticated) {
-    // ここに、認証済みの場合の処理を追加
-    return
-  }
-
-  const authInfo = defaultAuth;
-  const setting = defaultSetting;
-  const todos = defaultTodos;
-  const records = defaultRecords;
-  res.json({ authInfo, setting, todos, records });
-}
 
 export const deleteUser = async(req: Request, res: Response, next: NextFunction) => {
   try {
--- back/src/routes/auth/authRouter.ts
+++ back/src/routes/auth/authRouter.ts
@@ -1,13 +1,10 @@
 import { Router } from "express";
-import { getDefault } from "../../controllers/users/users";
 import { signIn, signOut, signUp, tokenCheck } from "../../controllers/auth/auth";
 import { signInValidator, userPostValidator } from "../../middlewares/validators/users";
 import { handleValidationResult } from "../../middlewares/validators/handleValidationResult";
 import { authCheckMiddleware } from "../../middlewares/auth/auth";
 
 const router = Router()
-
-router.get('/', getDefault);
 
 router.get('/check', tokenCheck);
 router.post('/signup', userPostValidator, handleValidationResult, signUp);
