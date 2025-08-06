49d43f585107fcd6140b4bc658a6c8a052c7870b
hayato_52b <a99246780@icloud.com>
2025-08-06 16:15:05 +0900

    ADD:front vitestをインストール

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,9 +1,14 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // vitestの設定
+  test: {
+  },
+
   plugins: [
     react(),
     tailwindcss(),
