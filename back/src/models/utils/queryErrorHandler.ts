cd51b253ce6e064dd6ba4b057556435ddac3a791
hayato_52b <a99246780@icloud.com>
2025-09-04 03:15:56 +0900

    remove:back DBクエリごとにdisconnectする処理を削除

--- back/src/models/utils/queryErrorHandler.ts
+++ back/src/models/utils/queryErrorHandler.ts
@@ -1,19 +1,17 @@
 import { PrismaClient } from "../../../generated/prisma";
 import { devLog } from "../../utils/dev/devLog";
 
-const prisma = new PrismaClient();
+export const prismaClient = new PrismaClient();
 
 // DBクエリ処理をエラーハンドラーでラップする
 export const dbQueryHandler = async < T, Args extends unknown[] >(
-  queryFn: (prisma: PrismaClient, ...args: Args) => Promise<T>,
+  queryFn: (prismaClient: PrismaClient, ...args: Args) => Promise<T>,
   ...args: Args
 ): Promise<T> => {
   try {
-    return await queryFn(prisma, ...args);
+    return await queryFn(prismaClient, ...args);
   } catch(err) {
     devLog(err)
     throw err;
-  } finally {
-    await prisma.$disconnect()
   }
 }
